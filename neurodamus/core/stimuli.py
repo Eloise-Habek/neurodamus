707b54ee24691cb125146ac44c7a2e335d4c5102
joseph-tharayil <tharayiljoe@gmail.com>
2025-09-16 13:15:18 +0200

    Adds numpy

--- neurodamus/core/stimuli.py
+++ neurodamus/core/stimuli.py
@@ -5,6 +5,7 @@ import logging
 from .random import RNG, gamma
 from neurodamus.core import NeuronWrapper as Nd
 from . import Neuron
+import numpy as np
 
 class SignalSource:
     def __init__(self, base_amp=0.0, *, delay=0, rng=None, represents_physical_electrode=False):
