d92ab7c6a624743d887dbadb25eeaacbe5f428d6
joseph-tharayil <tharayiljoe@gmail.com>
2025-09-16 15:10:27 +0200

    tpt

--- neurodamus/stimulus_manager.py
+++ neurodamus/stimulus_manager.py
@@ -800,7 +800,7 @@ class Extracellular(BaseStim):
 
         self.stimList = []  # sources go here
 
-        tpoints = target.get_point_list(cell_manager,sections='all')
+        tpoints = target.get_point_list(cell_manager,'all')
 
         print(tpoints)
 
