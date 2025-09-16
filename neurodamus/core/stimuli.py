eb671ab7f482ba1c20b1a8dc9f64f2f4fa552188
joseph-tharayil <tharayiljoe@gmail.com>
2025-09-16 16:07:39 +0200

    Working implementation of extracellular stimulus

--- neurodamus/core/stimuli.py
+++ neurodamus/core/stimuli.py
@@ -767,9 +767,7 @@ class ElectrodeSource(SignalSource):
         seg = section(x)
 
         scaleFac0, scaleFac1 = self.get_scale_factor(section, x) # Calculates the potential relative to the soma for the given segment, for both of the E fields
-        print('scaleFaco0 is ' + str(scaleFac0))
         stimVec0 = self.stim_vec.to_python()
-        print('max vec is ' + str(np.max(np.abs(stimVec0))))
         stimVec0 = self.apply_ramp(stimVec0) # Scales the sinusoid by the ramp-up and ramp-down windows
         stimVec0 *= scaleFac0 # Applies the calculated potential to the temporal waveform
 
