fedbc120de3d9ae64d54702cdf34c1218920f926
joseph-tharayil <tharayiljoe@gmail.com>
2025-09-16 12:40:28 +0200

    Fixes function name

--- neurodamus/stimulus_manager.py
+++ neurodamus/stimulus_manager.py
@@ -800,7 +800,7 @@ class Extracellular(BaseStim):
 
         self.stimList = []  # sources go here
 
-        tpoints = target.getPointList(cell_manager)
+        tpoints = target.get_point_list(cell_manager)
 
         posList = {}
 
