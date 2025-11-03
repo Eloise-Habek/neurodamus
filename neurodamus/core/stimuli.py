aa1ca9fc1ca30470d9025f8abb6fa086c286da6a
joseph-tharayil <tharayiljoe@gmail.com>
2025-11-03 15:11:27 +0100

    New print

--- neurodamus/core/stimuli.py
+++ neurodamus/core/stimuli.py
@@ -747,6 +747,7 @@ class ElectrodeSource(SignalSource):
         self.extracellulars.append(seg.extracellular.e)
 
         out = stim_vec_final.play(seg.extracellular._ref_e, self.time_vec,1)
+        print(out)
         self.extracellulars.append(out)
 
         return stim_vec_final, self.time_vec
