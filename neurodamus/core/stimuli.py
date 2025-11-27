4f46577449a79934e6efd0b4862d280183185e41
joseph-tharayil <tharayil@itis.swiss>
2025-11-27 14:25:07 +0100

    Test offset

--- neurodamus/core/stimuli.py
+++ neurodamus/core/stimuli.py
@@ -788,7 +788,7 @@ class ElectrodeSource(SignalSource):
 
         # Calculates distance between soma and each segment, since the ground is assumed to be at the soma
 
-        displacementVector = segpositions # Here, we set the reference at the point (0,0,0)
+        displacementVector = segpositions-np.array([10,10,10]) # Here, we set the reference at the point (0,0,0)
         displacementVector *= 1e-6 # Converts from um to m
 
         scaleFactor0 = np.dot(displacementVector, np.array([self.Ex_0,self.Ey_0, self.Ez_0]))
