6c69c61e5af123578812e53e9f02042fabcca003
joseph-tharayil <tharayiljoe@gmail.com>
2025-12-16 11:55:00 +0100

    Bug fix

--- neurodamus/core/stimuli.py
+++ neurodamus/core/stimuli.py
@@ -551,7 +551,7 @@ class ElectrodeSource(SignalSource):
         seg = section(x)
         out = self.stim_vec.play(seg.extracellular._ref_e, self.time_vec, 1)
 
-        self.extracellulars.append(stim_vec)
+        self.extracellulars.append(self.stim_vec)
         self.extracellulars.append(seg.extracellular)
         self.extracellulars.append(seg.extracellular.e)
         self.extracellulars.append(out)
